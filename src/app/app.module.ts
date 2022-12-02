9f7cfc0d6997bf51749e82355cdd413abb9623f1
Youssef Alamrousy <Youssef.amrousy@gmail.com>
2022-12-02 02:20:39 +0200

    files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotfoundComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
